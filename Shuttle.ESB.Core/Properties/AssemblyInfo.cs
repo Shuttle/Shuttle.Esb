830bdbb060e34eb8e0665e0befa17d362982b3a1
eben-roux <me@ebenroux.co.za>
2015-05-22 08:13:05 +0200

    - released v3.6.0

--- Shuttle.ESB.Core/Properties/AssemblyInfo.cs
+++ Shuttle.ESB.Core/Properties/AssemblyInfo.cs
@@ -18,11 +18,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.ESB.Core for .NET Framework 4.5.1")]
 #endif
 
-[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.6.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.ESB")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("3.5.0")]
+[assembly: AssemblyInformationalVersion("3.6.0")]
 [assembly: ComVisible(false)]
 
