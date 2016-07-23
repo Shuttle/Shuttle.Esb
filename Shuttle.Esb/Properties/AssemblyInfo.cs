e0ded9c654bf4eaf781a85b10dd0e2cb29977ef8
Eben <me@ebenroux.co.za>
2016-07-23 08:58:25 +0200

    - v6.2.0

--- Shuttle.Esb/Properties/AssemblyInfo.cs
+++ Shuttle.Esb/Properties/AssemblyInfo.cs
@@ -26,10 +26,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("6.1.3.0")]
+[assembly: AssemblyVersion("6.2.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("6.1.3")]
+[assembly: AssemblyInformationalVersion("6.2.0")]
 [assembly: ComVisible(false)]
