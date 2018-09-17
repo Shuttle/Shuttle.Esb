267fc07f5cb6c56404da99e995fc1aa6bcc7b3b8
eben-roux <me@ebenroux.co.za>
2018-09-17 17:34:58 +0200

    - v10.1.0

--- Shuttle.Esb/Properties/AssemblyInfo.cs
+++ Shuttle.Esb/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.10.0")]
+[assembly: AssemblyVersion("10.1.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.10")]
+[assembly: AssemblyInformationalVersion("10.1.0")]
 [assembly: ComVisible(false)]
