450ded791ef4665740896e42ffa9179ca20c3478
Eben <me@ebenroux.co.za>
2022-04-09 15:38:29 +0200

    - package upgrade

--- Shuttle.Esb/Properties/AssemblyInfo.cs
+++ Shuttle.Esb/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyVersion("12.0.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2022, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("12.0.0")]
+[assembly: AssemblyInformationalVersion("12.0.1")]
 [assembly: ComVisible(false)]
