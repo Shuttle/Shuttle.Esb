1926f800c3b30c57cd25e5bcbde1d6a095bf822d
Eben <me@ebenroux.co.za>
2022-11-27 21:21:57 +0200

    - apckage upgrade

--- Shuttle.Esb/Properties/AssemblyInfo.cs
+++ Shuttle.Esb/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("13.2.0.0")]
+[assembly: AssemblyVersion("13.2.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2022, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("13.2.0")]
+[assembly: AssemblyInformationalVersion("13.2.1")]
 [assembly: ComVisible(false)]
