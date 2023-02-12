b39267e5f18392e6035d2127c0f6460cdc2e87b5
Eben <me@ebenroux.co.za>
2023-02-12 09:51:44 +0200

    - v14.0.0

--- Shuttle.Esb/Properties/AssemblyInfo.cs
+++ Shuttle.Esb/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("13.2.1.0")]
-[assembly: AssemblyCopyright("Copyright (c) 2022, Eben Roux")]
+[assembly: AssemblyVersion("14.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) 2023, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("13.2.1")]
+[assembly: AssemblyInformationalVersion("14.0.0")]
 [assembly: ComVisible(false)]
